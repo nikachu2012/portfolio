57c43079ca9249d95db2b6c3ff272e4df2f350ff
nikachu2012 <webmaster@nikachu.net>
2024-06-27 20:03:24 +0900

    [add] 下線とボーダーを修正

--- app/component/section.tsx
+++ app/component/section.tsx
@@ -3,7 +3,7 @@ type Props = {
 }
 
 export default function Section(props: Props) {
-    return <h2 className={"border-b-[1px] border-b-black"}>
+    return <h2 className={"border-b-[1px] border-b-[#d3d3d3] text-3xl font-bold my-3 py-2"}>
         {props.text}
     </h2>
 }
