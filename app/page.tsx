232b2eecf9fc31447de63b7744b79f563161e0d4
nikachu2012 <webmaster@nikachu.net>
2024-06-27 19:57:49 +0900

    [add] ページ幅の変更

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Profile from "./component/profile";
 export default function Home() {
   return (<>
     <Header />
-    <main className="w-full md:w-[1200px] m-auto">
+    <main className="w-full md:w-[1000px] m-auto">
       {/* main element */}
       <Profile />
 
