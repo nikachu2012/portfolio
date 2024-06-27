8ee1d711abd82a1dd7da22aa6eac8de8abd37fff
nikachu2012 <webmaster@nikachu.net>
2024-06-27 19:57:38 +0900

    [add] 日本語フォントをNoto Sans JPに変更

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Noto_Sans_JP } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const notosans = Noto_Sans_JP({ subsets: ["latin"], variable: "--font-notosans" })
 
 export const metadata: Metadata = {
   title: {
@@ -35,7 +36,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.variable} ${notosans.variable}`} style={{ "fontVariantLigatures": "none" }}>{children}</body>
     </html>
   );
 }
