117d0393fe04e88a390b2eb72ef27a96ca2725bb
nikachu2012 <webmaster@nikachu.net>
2024-07-10 20:12:57 +0900

    [fix] 正しく中央に寄せるために行間を1に固定

--- app/component/socialIcon.tsx
+++ app/component/socialIcon.tsx
@@ -10,7 +10,7 @@ export default function SocialIcon(props: Props) {
     return <a href={props.href} target="_blank">
         <div className="w-[100px] h-[100px] flex justify-center items-center hover:bg-gray-200 flex-col cursor-pointer select-none">
             <Image src={props.img} alt={props.text} width={48} height={48} />
-            <span className="text-lg mt-1">{props.text}</span>
+            <span className="text-lg mt-2 leading-none">{props.text}</span>
         </div>
     </a>
 
