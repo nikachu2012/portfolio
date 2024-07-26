0be0510dd76a4bb18375b0724a3783d234a8c6b7
nikachu2012 <webmaster@nikachu.net>
2024-07-26 23:50:03 +0900

    [update] 黒を乗せるのではなく黒を透かせるように変更

--- app/section/header.tsx
+++ app/section/header.tsx
@@ -5,11 +5,11 @@ import nikachu from "../../public/nikachu.png"
 export default function Header() {
     return <header>
         <div className="w-full h-[150px] md:h-[250px] relative">
-            <div className="w-full h-full absolute blur-[2px]">
-                <Image src={HeaderImage} alt="Header" className="w-full h-full object-cover" />
+            <div className="w-full h-full absolute bg-black">
+                <Image src={HeaderImage} alt="Header" className="w-full h-full object-cover opacity-40 blur-[2px]" height={250} />
             </div>
-            <div className="w-full h-full absolute bg-[#0000007f] flex flex-col items-center justify-center gap-3 md:gap-5">
-                <Image src={nikachu} alt="nikachu icon" className="w-[70px] h-[70px] md:w-[140px] md:h-[140px] rounded-full bg-white" />
+            <div className="w-full h-full absolute flex flex-col items-center justify-center gap-3 md:gap-5">
+                <Image src={nikachu} alt="nikachu icon" className="w-[70px] h-[70px] md:w-[140px] md:h-[140px] rounded-full bg-white" width={140} height={140} />
                 <h1 className="font-bold text-white text-xl md:text-3xl">nikachu2012 / So Ogino</h1>
 
             </div>
