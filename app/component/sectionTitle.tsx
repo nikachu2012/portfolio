a87f713fdd83c5f04d28db33c9c752b556279789
nikachu2012 <webmaster@nikachu.net>
2025-01-13 16:55:03 +0900

    [add] セクションタイトルにidを付けてジャンプできるように変更

--- app/component/sectionTitle.tsx
+++ app/component/sectionTitle.tsx
@@ -3,7 +3,7 @@ type Props = {
 }
 
 export default function Section(props: Props) {
-    return <h2 className={"border-b-[1px] border-b-[#d3d3d3] text-3xl font-bold my-3 py-2 leading-none"}>
+    return <h2 className={"border-b-[1px] border-b-[#d3d3d3] text-3xl font-bold my-3 py-2 leading-none"} id={props.text.toLowerCase()}>
         {props.text}
     </h2>
 }
